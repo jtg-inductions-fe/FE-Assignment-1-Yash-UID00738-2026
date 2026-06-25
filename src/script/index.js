80ec4cc78c0870e96686273857d7cdd414725dd6
yashgoel-jpg <yash.goel@joshtechnologygroup.com>
2026-06-25 17:54:33 +0530

    implemented dynamic company travel data display logic

--- src/script/index.js
+++ src/script/index.js
@@ -1,5 +1,7 @@
 import headerInit from './header.js';
+import travelPointInit from './travelPoint.js';
 
 document.addEventListener('DOMContentLoaded', function () {
     headerInit();
+    travelPointInit();
 });
