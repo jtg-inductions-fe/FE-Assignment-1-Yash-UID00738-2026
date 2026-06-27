78abe01f9d7f6109df7859e2777e33ca620f6b05
yashgoel-jpg <yash.goel@joshtechnologygroup.com>
2026-06-27 18:30:06 +0530

    implemented testimonials sections using splide and pixel perfected in mobile devie

--- src/script/testimonials.js
+++ src/script/testimonials.js
@@ -3,7 +3,11 @@ import '@splidejs/splide/css';
 
 const testimonialsInit = () => {
     const splideInit = () => {
-        new Splide('.splide').mount();
+        new Splide('.splide', {
+            type: 'loop', // Infinite scroll
+            perPage: 1, // Show one testimonial at a time
+            pagination: true, // Auto-generates the pagination dots at the bottom
+        }).mount();
     };
 
     splideInit();
