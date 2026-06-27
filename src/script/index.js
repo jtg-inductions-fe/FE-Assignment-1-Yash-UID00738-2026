3c84dfc0ab56eca4f639b0939f27e23cd6bca70e
yashgoel-jpg <yash.goel@joshtechnologygroup.com>
2026-06-27 18:30:06 +0530

    implemented testimonials sections using splide and pixel perfected in mobile devie

--- src/script/index.js
+++ src/script/index.js
@@ -1,7 +1,9 @@
 import { headerInit } from './header.js';
 import { travelPointInit } from './travelPoint.js';
+import { testimonialsInit } from './testimonials.js';
 
 document.addEventListener('DOMContentLoaded', function () {
     headerInit();
     travelPointInit();
+    testimonialsInit();
 });
